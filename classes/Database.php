33923a18d7047986dc8ff10634d633bd29ae88c5
Sagar Starter's <sagarstarters@gmail.com>
2026-04-08 15:01:53 +0530

    Synchronized database timezone with PHP timezone for correct message timestamps

--- classes/Database.php
+++ classes/Database.php
@@ -10,11 +10,12 @@ class Database {
     private function __construct() {
         try {
             $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_CHARSET;
+            $tzOffset = date('P');
             $options = [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES => false,
-                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci"
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci; SET time_zone = '$tzOffset';"
             ];
             $this->pdo = new PDO($dsn, DB_USER, DB_PASS, $options);
         } catch (PDOException $e) {
