888450f32ecd322ed38d92e1c40d533d10034976
Sagar Starter's <sagarstarters@gmail.com>
2026-09-20 13:10:03 +0530

    debug: check gd support on server

--- api/chatbot/debug_flow_db.php
+++ api/chatbot/debug_flow_db.php
@@ -1,21 +1,10 @@
 <?php
-$urls = [
-    'cat11' => 'https://www.sagarstarters.com/shop.php?category=11',
-    'cat7' => 'https://www.sagarstarters.com/shop.php?category=7',
-    'cat8' => 'https://www.sagarstarters.com/shop.php?category=8',
-];
-
-$res = [];
-foreach ($urls as $k => $u) {
-    $h = @file_get_contents($u, false, stream_context_create([
-        'http' => ['timeout' => 5, 'header' => "User-Agent: Mozilla/5.0\r\n", 'follow_location' => 1]
-    ]));
-    if ($h) {
-        preg_match_all('/uploads\/media\/images\/[a-zA-Z0-9_\-\.]+/i', $h, $m);
-        $res[$k] = array_values(array_unique($m[0] ?? []));
-    } else {
-        $res[$k] = 'failed to fetch';
-    }
-}
+$gd = extension_loaded('gd');
+$info = $gd ? gd_info() : [];
 header('Content-Type: application/json');
-echo json_encode($res, JSON_PRETTY_PRINT);
+echo json_encode([
+    'gd' => $gd,
+    'webp_support' => $info['WebP Support'] ?? false,
+    'jpeg_support' => $info['JPEG Support'] ?? false,
+    'png_support' => $info['PNG Support'] ?? false
+], JSON_PRETTY_PRINT);
