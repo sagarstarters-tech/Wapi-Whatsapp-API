c7089556d76e57cd19e266678197ab09ac97d85f
Sagar Starter's <sagarstarters@gmail.com>
2026-09-20 14:05:36 +0530

    test: test knowledge search queries

--- api/chatbot/inspect_kb.php
+++ api/chatbot/inspect_kb.php
@@ -8,13 +8,18 @@ try {
     $db = Database::getInstance();
     $res = [];
 
-    $chunks_cols = $db->fetchAll("DESCRIBE ai_kb_chunks");
-    $qa_cols = $db->fetchAll("DESCRIBE ai_kb_qa_pairs");
-    $urls_cols = $db->fetchAll("DESCRIBE ai_kb_urls");
+    // Test knowledge search
+    $q1 = AIKnowledgeBase::searchChunks(1, '1 hp submersible motor starter buy link', 5);
+    $q2 = AIKnowledgeBase::searchChunks(1, 'mujhe is product ka link do 1 hp aur 220v ki hai', 5);
+    $q3 = AIKnowledgeBase::searchChunks(1, 'product kharidne ka link', 5);
+
     echo json_encode([
-        'chunks_cols' => $chunks_cols,
-        'qa_cols' => $qa_cols,
-        'urls_cols' => $urls_cols
+        'q1_count' => count($q1),
+        'q1_top_snippet' => !empty($q1) ? substr($q1[0]['content'], 0, 300) : null,
+        'q2_count' => count($q2),
+        'q2_top_snippet' => !empty($q2) ? substr($q2[0]['content'], 0, 300) : null,
+        'q3_count' => count($q3),
+        'q3_top_snippet' => !empty($q3) ? substr($q3[0]['content'], 0, 300) : null,
     ], JSON_PRETTY_PRINT);
     exit;
 } catch (Throwable $t) {
